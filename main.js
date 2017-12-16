4dcc6352bd82f9c219c74483132b7409426e2cde
nickjdb <34308632+nickjdb@users.noreply.github.com>
2017-12-16 12:36:36 +1100

    gallery add

--- main.js
+++ main.js
@@ -92,5 +92,25 @@ $(".login-button").click(function(){
   });
 
 
+// this is for the popup gallery
+
+  $('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Nick Della </small>';
+			}
+		}
+	});
+
 
 });
